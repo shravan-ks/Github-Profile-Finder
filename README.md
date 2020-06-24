30047f13ea077224fe44f3385cb6c12a673abba9
shravan <shravan2967@gmail.com>
2020-06-24 13:55:53 +0530

    Intial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Github Finder
+
+> React app to search Github profiles. This app uses the Context API along with the useContext and useReducer hooks for state management.
+
+## Usage
+
+### `npm install`
+
+### `npm start`
+
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About</h1>
+      <p>App to search Github users built on React, using Github Api</p>
+    </Fragment>
+  );
+};
+
+export default About;
